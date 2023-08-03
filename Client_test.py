0cc19c9ce0b7cd1a3f40943da9b061b9b34c0733
Miguel <93895318+bendItLikeBeckham@users.noreply.github.com>
2023-08-03 13:24:14 +0800

    Update Client_test.py
    Fixed leave and quit

--- Client_test.py
+++ Client_test.py
@@ -33,12 +33,16 @@ def main():
             print("Input Syntax commands:")
             print("/join <server_ip_add> <port>")
             print("/leave")
+            print("/quit")
             print("/register <handle>")
             print("/all <message>")
             print("/msg <handle> <message>")
             print("/?")
         elif parts[0].lower() == "/leave":
             print("Error: Disconnection failed. Please connect to the server first")
+        elif parts[0].lower() =="/quit":
+            print("Disconnecting...")
+            client_socket.close()
         else:
             print("Invalid command format. Usage: /join <ip_address> <port> or /?")
             
@@ -52,16 +56,49 @@ def main():
         while True:
             message = input()
             client_socket.sendall(message.encode('utf-8'))
-            if message == "/leave":
+            if message == "/quit":
                 break
             elif message == "/?": 
                 print("Input Syntax commands:")
                 print("/join <server_ip_add> <port>")
                 print("/leave")
+                print("/quit")
                 print("/register <handle>")
                 print("/all <message>")
                 print("/msg <handle> <message>")
                 print("/?")
+            elif message == "/leave": 
+                while True:
+                    command = input()
+                    parts = command.strip().split()
+                    if len(parts) == 3 and parts[0].lower() == "/join":
+                        ip_address = parts[1]
+                        port = int(parts[2])
+                        try:
+                            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            client_socket.connect((ip_address, port))
+                            print("Connected to the server.")
+                            break  
+                        except Exception as e:
+                            print(f"Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number {e}")               
+                    elif parts[0].lower() == "/?":
+                        print("Input Syntax commands:")
+                        print("/join <server_ip_add> <port>")
+                        print("/leave")
+                        print("/register <handle>")
+                        print("/all <message>")
+                        print("/msg <handle> <message>")
+                        print("/?")
+                    elif parts[0].lower() == "/leave":
+                        print("Error: Disconnection failed. Please connect to the server first")
+                    else:
+                        print("Invalid command format. Usage: /join <ip_address> <port> or /?")                        
+                receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+                receive_thread.start()
+
+                receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+                receive_thread.start()
+                
     except KeyboardInterrupt:
         pass
 
